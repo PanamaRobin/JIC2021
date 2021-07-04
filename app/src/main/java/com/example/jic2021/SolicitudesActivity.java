b3b943ead016933ca208c5aed29a97a923fd96a9
Jorge Herrera <jlherrera22@live.com>
2021-07-04 12:43:12 -0500

    Avance en la vista de solicitudes. TODO: -Finalizar boton flotante. -Adjuntar items a la vista. -Cuando no hayan solicitudes, mostrar un textView que lo informe.

--- app/src/main/java/com/example/jic2021/SolicitudesActivity.java
+++ app/src/main/java/com/example/jic2021/SolicitudesActivity.java
@@ -3,6 +3,8 @@ package com.example.jic2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SolicitudesActivity extends AppCompatActivity {
 
@@ -10,11 +12,25 @@ public class SolicitudesActivity extends AppCompatActivity {
      * Vista de Solicitudes, esta seccion le toca a Jorge Herrera.
      * Por favor agregar comentarios al codigo para poder visualizarlo mejor cuando haya
      * que juntarlo.
-     * @param savedInstanceState
      */
+/*
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitudes);
+
+
     }
 }
