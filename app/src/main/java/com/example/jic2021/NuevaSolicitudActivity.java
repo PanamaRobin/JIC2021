6d7c9db2fc84ba8205a6641234f88b96aa2e92af
Nic19 <nicolas.samaniego97@gmail.com>
2021-07-08 13:57:43 -0500

    Manejo de camara Hay ciertos codigos que me debo modificar para cuando se desee enviar las imagenes a la base datoss.

--- app/src/main/java/com/example/jic2021/NuevaSolicitudActivity.java
+++ app/src/main/java/com/example/jic2021/NuevaSolicitudActivity.java
@@ -1,11 +1,28 @@
 package com.example.jic2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NuevaSolicitudActivity extends AppCompatActivity {
-
+    private ImageView imagen;
     /**
      * Nueva Solicitud, esta seccion le toca a Jorge Morales.
      * Por favor agregar comentarios al codigo para poder visualizarlo mejor cuando haya
@@ -16,5 +33,75 @@ public class NuevaSolicitudActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_solicitud);
+
+        imagen=(ImageView)findViewById(R.id.bt_camara);
+        if (ContextCompat.checkSelfPermission(NuevaSolicitudActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(NuevaSolicitudActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(NuevaSolicitudActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
     }
+
+
+    //fuente de donde se obtuvo el codigo: https://developer.android.com/training/camera/photobasics?hl=es-419
+//me falta el area cuando se tenga que enviar la foto a la base de datos
+//tomar la foto.
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void tomarFotos(View view) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    String mCurrentPhotoPath;
+    //guardar foto en dispositivo movil. Esto no es necesario.
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",
+                storageDir);
+
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    //mostrar la fotos.
+    static final int REQUEST_IMAGE_CAPTURE=1;
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+
 }
