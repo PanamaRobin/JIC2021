ecfa11e7713187fac3d5ab1997791cca773aa95e
josiasalx <josiasalx11@gmail.com>
2021-07-12 11:04:20 -0500

    Se agregaron comentarios a los códigos.

--- app/src/main/java/com/example/jic2021/MainActivity.java
+++ app/src/main/java/com/example/jic2021/MainActivity.java
@@ -1,39 +1,44 @@
 package com.example.jic2021;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 Button iniciar, registrarse, olvidopass;
-    /**
-     * Login, esta seccion le toca a Josias Aponte.
-     * Por favor agregar comentarios al codigo para poder visualizarlo mejor cuando haya
-     * que juntarlo.
-     * @param savedInstanceState
-     */
+EditText user, pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        iniciar=findViewById(R.id.iniciar);
+        user=findViewById(R.id.et_loguser);/**Se conecta la variable creada con el id del edittext correspondiente*/
+        pass=findViewById(R.id.et_logcontraseña);/**Se conecta la variable creada con el id del edittext correspondiente*/
+        iniciar=findViewById(R.id.iniciar);/**Se conecta la variable creada con el id del botón correspondiente*/
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,RegistroActivity.class));
             }
-        });
-        registrarse=findViewById(R.id.logregistro);
+        });/**aquí se le asigna un OnClick, que es un evento que se dará cuando se pulse el botón. Aquí,
+         al apretar el botón de iniciar sesión, cambia de vista. La vista a la cual debe ir sería la de Inicio, que es la del listado de solicitudes.
+         Pero cómo no se encuentra en mi rama, lo coloqué que vaya a la de registro
+         únicamente para que vea el funcionamiento. Cuando se conecten las 4 ramas, entonces es cambia a:
+         startActivity(new(Intent(MainActivity.this,SolicitudesActivity.class));*/
+        registrarse=findViewById(R.id.logregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent (MainActivity.this,RegistroActivity.class));
             }
         });
-
+        /**aquí se le asigna un OnClick, que es un evento que se dará cuando se pulse el botón. Lo que hace es que cuando el usuario aprete el botón
+         * registrarse, cambie de la vista de iniciar sesión a la vista de
+         * registrarse*/
     }
 }
--- app/src/main/java/com/example/jic2021/RegistroActivity.java
+++ app/src/main/java/com/example/jic2021/RegistroActivity.java
@@ -1,12 +1,12 @@
 package com.example.jic2021;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 public class RegistroActivity extends AppCompatActivity {
 Button regregistro, cancelar;
 
@@ -15,19 +15,24 @@ protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_registro);
 
-        regregistro=findViewById(R.id.regregistro);
+        regregistro=findViewById(R.id.regregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
         regregistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegistroActivity.this,MainActivity.class));
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                "Registro exitoso", Toast.LENGTH_SHORT);
+                toast1.show();
             }
-        });
-        cancelar=findViewById(R.id.regcancelar);
+        });/**Aquí, cuando el usuario ha llenado los campos debidamente, apretará el botón de Registrarse. Una vez apretado, se envía un mensaje corto
+     notificándole que se ha registrado exitosamente e inmediatamente lo lleva a la vista de Iniciar Sesión.*/
+        cancelar=findViewById(R.id.regcancelar);/**Se conecta la variable creada con el id del botón correspondiente*/
         cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegistroActivity.this,MainActivity.class));
             }
-        });
+        });/**En caso tal de que el usuario no quiera crear una cuenta, puede darle al botón cancelar y lo llevará a la pantalla de Iniciar Sesión*/
 }
 }
