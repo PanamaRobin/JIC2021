269f226bc73a3f9935fa16d3ddcb2711ac170209
jlher <jlherrera22@live.com>
2021-07-11 23:12:00 -0500

    Finalizado botón flotante y item del recycler. TODO: anclar el item con el adapter dentro del recycler.

--- app/src/main/java/com/example/jic2021/MainActivity.java
+++ app/src/main/java/com/example/jic2021/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.jic2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +15,25 @@ public class MainActivity extends AppCompatActivity {
      * que juntarlo.
      * @param savedInstanceState
      */
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(this,SolicitudesActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/jic2021/SolicitudesActivity.java
+++ app/src/main/java/com/example/jic2021/SolicitudesActivity.java
@@ -3,8 +3,11 @@ package com.example.jic2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SolicitudesActivity extends AppCompatActivity {
 
@@ -13,24 +16,48 @@ public class SolicitudesActivity extends AppCompatActivity {
      * Por favor agregar comentarios al codigo para poder visualizarlo mejor cuando haya
      * que juntarlo.
      */
-/*
-    private val rotateOpen: Animation by lazy {
-        AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)
-    }
-    private val rotateClose: Animation by lazy {
-        AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)
-    }
-    private val fromBottom: Animation by lazy {
-        AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)
-    }
-    private val toBottom: Animation by lazy {
-        AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)
-    }*/
+
+    FloatingActionButton float1, float2, float3;
+    Animation closeAnim, openAnim, fromBottom, toBottom;
+
+    boolean isVisible = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitudes);
 
+        //Floating action buttons
+        float1 = (FloatingActionButton) findViewById(R.id.new_button);
+        float2 = (FloatingActionButton) findViewById(R.id.profile_button);
+        float3 = (FloatingActionButton) findViewById(R.id.camera_button);
+
+        //Animations
+        closeAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim);
+        openAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
+
+        float1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateFab();
+            }
+        });
+    }
+
+    private void animateFab(){
+        if(isVisible) {
+            float1.startAnimation(openAnim);
+            float2.startAnimation(fromBottom);
+            float3.startAnimation(fromBottom);
+            isVisible = false;
+        }else{
+            float1.startAnimation(closeAnim);
+            float2.startAnimation(toBottom);
+            float3.startAnimation(toBottom);
+            isVisible = true;
+        }
 
     }
 }
