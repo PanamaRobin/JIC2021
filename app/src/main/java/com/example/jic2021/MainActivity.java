0d208c3a58e17ed2922890c57d2d3bbc19710d5e
josiasalx <josiasalx11@gmail.com>
2021-07-17 23:48:28 -0500

    Se agregó una vista y actividad que le notifica al usuario que el registro ha sido exitoso.

--- app/src/main/java/com/example/jic2021/MainActivity.java
+++ app/src/main/java/com/example/jic2021/MainActivity.java
@@ -2,41 +2,76 @@ package com.example.jic2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
-Button iniciar, registrarse, olvidopass;
+Button  registrarse, olvidopass;
 EditText user, pass;
+ImageButton salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        user=findViewById(R.id.et_loguser);/**Se conecta la variable creada con el id del edittext correspondiente*/
-        pass=findViewById(R.id.et_logcontraseña);/**Se conecta la variable creada con el id del edittext correspondiente*/
-        iniciar=findViewById(R.id.iniciar);/**Se conecta la variable creada con el id del botón correspondiente*/
-        iniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,RegistroActivity.class));
-            }
-        });/**aquí se le asigna un OnClick, que es un evento que se dará cuando se pulse el botón. Aquí,
-         al apretar el botón de iniciar sesión, cambia de vista. La vista a la cual debe ir sería la de Inicio, que es la del listado de solicitudes.
-         Pero cómo no se encuentra en mi rama, lo coloqué que vaya a la de registro
-         únicamente para que vea el funcionamiento. Cuando se conecten las 4 ramas, entonces es cambia a:
-         startActivity(new(Intent(MainActivity.this,SolicitudesActivity.class));*/
-        registrarse=findViewById(R.id.logregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
+        user = findViewById(R.id.et_loguser);/**Se conecta la variable creada con el id del edittext correspondiente*/
+        pass = findViewById(R.id.et_logcontraseña);/**Se conecta la variable creada con el id del edittext correspondiente*/
+
+        registrarse = findViewById(R.id.logregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent (MainActivity.this,RegistroActivity.class));
+                startActivity(new Intent(MainActivity.this, RegistroActivity.class));
             }
         });
+
+        salir = findViewById(R.id.salir);
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogo();
+            }
+        });
+    }
+       private void mostrarDialogo(){
+           new AlertDialog.Builder(this)
+                   .setTitle("Salir")
+                   .setMessage("¿Desea salir de la aplicación?")
+                   .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           finish();
+                       }
+                   })
+                   .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           Log.d("Mensaje", "Se canceló la acción");
+                       }
+                   })
+                   .show();
+        }
+
+        public void Enviar (View v){
+        if(user.getText().toString().isEmpty()){
+            Toast.makeText(this, "Campo cédula vacío", Toast.LENGTH_LONG).show();
+        }else{
+            if(pass.getText().toString().isEmpty()){
+            Toast.makeText(this,"Campo contraseña vacío", Toast.LENGTH_LONG).show();
+        }else{
+          Toast.makeText(this, "Inicio de Sesion exitoso", Toast.LENGTH_LONG).show();
+            }
+        }
+
         /**aquí se le asigna un OnClick, que es un evento que se dará cuando se pulse el botón. Lo que hace es que cuando el usuario aprete el botón
          * registrarse, cambie de la vista de iniciar sesión a la vista de
          * registrarse*/
--- app/src/main/java/com/example/jic2021/RegistroActivity.java
+++ app/src/main/java/com/example/jic2021/RegistroActivity.java
@@ -4,35 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
 public class RegistroActivity extends AppCompatActivity {
-Button regregistro, cancelar;
+    Button regregistro, cancelar;
+    EditText cedula, correo, telefono, pass;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_registro);
-
-        regregistro=findViewById(R.id.regregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
-        regregistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegistroActivity.this,MainActivity.class));
-                Toast toast1 =
-                        Toast.makeText(getApplicationContext(),
-                                "Registro exitoso", Toast.LENGTH_SHORT);
-                toast1.show();
-            }
-        });/**Aquí, cuando el usuario ha llenado los campos debidamente, apretará el botón de Registrarse. Una vez apretado, se envía un mensaje corto
-     notificándole que se ha registrado exitosamente e inmediatamente lo lleva a la vista de Iniciar Sesión.*/
-        cancelar=findViewById(R.id.regcancelar);/**Se conecta la variable creada con el id del botón correspondiente*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        cedula = findViewById(R.id.et_regcedula);
+        correo = findViewById(R.id.et_regcorreo);
+        telefono = findViewById(R.id.et_regtelefono);
+        pass = findViewById(R.id.et_regcontraseña);
+        cancelar = findViewById(R.id.regcancelar);/**Se conecta la variable creada con el id del botón correspondiente*/
         cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegistroActivity.this,MainActivity.class));
+                startActivity(new Intent(RegistroActivity.this, MainActivity.class));
             }
-        });/**En caso tal de que el usuario no quiera crear una cuenta, puede darle al botón cancelar y lo llevará a la pantalla de Iniciar Sesión*/
-}
+        });
+    }
+
+    public void Registrar(View v) {
+        if (cedula.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Campo cédula vacío", Toast.LENGTH_LONG).show();
+        } else {
+            if (correo.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Campo correo vacío", Toast.LENGTH_LONG).show();
+            } else {
+                if (telefono.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Campo teléfono vacío", Toast.LENGTH_LONG).show();
+                } else {
+                    if (pass.getText().toString().isEmpty()) {
+                        Toast.makeText(this, "Campo contraseña vacío", Toast.LENGTH_LONG).show();
+                    } else {
+                        if (!validarEmail(correo.getText().toString())) {
+                            Toast.makeText(this, "Correo incorrecto", Toast.LENGTH_LONG).show();
+                        } else {
+                            startActivity(new Intent(RegistroActivity.this, ExitosoActivity.class));
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
 }
