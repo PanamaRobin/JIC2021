2e0ea54424b06d12e5aba7a1345db2baece3d046
josiasalx <josiasalx11@gmail.com>
2021-07-19 15:20:20 -0500

    Se agregó el API.

--- app/src/main/java/com/example/jic2021/MainActivity.java
+++ app/src/main/java/com/example/jic2021/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -14,17 +15,23 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-Button  registrarse, olvidopass;
-EditText user, pass;
-ImageButton salir;
+import com.example.jic2021.entities.Usuario;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+Button  salir, registrarse, olvidopass;
+EditText logcorreo, logpass;
+    public String base = "user" + ":" + "admin";
+    public final String authHeader = "Basic " + Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        user = findViewById(R.id.et_loguser);/**Se conecta la variable creada con el id del edittext correspondiente*/
-        pass = findViewById(R.id.et_logcontraseña);/**Se conecta la variable creada con el id del edittext correspondiente*/
+        logcorreo = findViewById(R.id.et_logcorreo);/**Se conecta la variable creada con el id del edittext correspondiente*/
+        logpass = findViewById(R.id.et_logcontraseña);/**Se conecta la variable creada con el id del edittext correspondiente*/
 
         registrarse = findViewById(R.id.logregistro);/**Se conecta la variable creada con el id del botón correspondiente*/
         registrarse.setOnClickListener(new View.OnClickListener() {
@@ -33,47 +40,63 @@ ImageButton salir;
                 startActivity(new Intent(MainActivity.this, RegistroActivity.class));
             }
         });
-
-        salir = findViewById(R.id.salir);
-        salir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mostrarDialogo();
-            }
-        });
     }
-       private void mostrarDialogo(){
-           new AlertDialog.Builder(this)
-                   .setTitle("Salir")
-                   .setMessage("¿Desea salir de la aplicación?")
-                   .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           finish();
-                       }
-                   })
-                   .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           Log.d("Mensaje", "Se canceló la acción");
-                       }
-                   })
-                   .show();
+public void agregar(View v){
+        if(validar()){
+            Toast.makeText(this, "Procesando...", Toast.LENGTH_LONG).show();
+            inicioUsuario();
         }
-
-        public void Enviar (View v){
-        if(user.getText().toString().isEmpty()){
+}
+        /**public void Enviar (View v){
+        if(logcorreo.getText().toString().isEmpty()){
             Toast.makeText(this, "Campo cédula vacío", Toast.LENGTH_LONG).show();
         }else{
-            if(pass.getText().toString().isEmpty()){
+            if(logpass.getText().toString().isEmpty()){
             Toast.makeText(this,"Campo contraseña vacío", Toast.LENGTH_LONG).show();
         }else{
           Toast.makeText(this, "Inicio de Sesion exitoso", Toast.LENGTH_LONG).show();
+          inicioUsuario();
             }
         }
+    }*/
+    public void inicioUsuario(){
+        Usuario user = new Usuario();
+        user.setEmail(AES.encrypt(logcorreo.getText().toString(),"64aes64"));
+        user.setContrasena(AES.encrypt(logpass.getText().toString(),"64aes64"));
+        Call<Usuario> calluser = API.conexionUsuario().checkUsuario(user, authHeader);
+        calluser.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                try{
+                    if(response.isSuccessful()){
+                        Usuario u= response.body();
+                        if(u.isActivo()) {
+                            startActivity(new Intent(MainActivity.this, SolicitudesActivity.class));
+                        }
+                    }
+                }catch(NullPointerException e){
 
-        /**aquí se le asigna un OnClick, que es un evento que se dará cuando se pulse el botón. Lo que hace es que cuando el usuario aprete el botón
-         * registrarse, cambie de la vista de iniciar sesión a la vista de
-         * registrarse*/
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+
+            }
+        });
+    }
+    public boolean validar(){
+        boolean retorno = true;
+        String c1 = logcorreo.getText().toString();
+        String c2 = logpass.getText().toString();
+        if(c1.isEmpty()){
+            logcorreo.setError("Introduzca su correo!");
+            retorno=false;
+        }
+        if(c2.isEmpty()){
+            logpass.setError("Introduzca su contraseña!");
+            retorno=false;
+        }
+        return retorno;
     }
 }
--- app/src/main/java/com/example/jic2021/RegistroActivity.java
+++ app/src/main/java/com/example/jic2021/RegistroActivity.java
@@ -1,25 +1,36 @@
 package com.example.jic2021;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.jic2021.entities.Usuario;
+
 import java.util.regex.Pattern;
 
-public class RegistroActivity extends AppCompatActivity {
-    Button regregistro, cancelar;
-    EditText cedula, correo, telefono, pass;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class RegistroActivity extends AppCompatActivity {
+    Button cancelar;
+    EditText cedula, correo, telefono, pass, nombre;
+    public String base = "user" + ":" + "admin";
+    public final String authHeader = "Basic " + Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        nombre = findViewById(R.id.et_nombre);
         cedula = findViewById(R.id.et_regcedula);
         correo = findViewById(R.id.et_regcorreo);
         telefono = findViewById(R.id.et_regtelefono);
@@ -33,32 +44,117 @@ public class RegistroActivity extends AppCompatActivity {
         });
     }
 
-    public void Registrar(View v) {
-        if (cedula.getText().toString().isEmpty()) {
+    /**public void Registrar(View v){
+        if(cedula.getText().toString().isEmpty()){
             Toast.makeText(this, "Campo cédula vacío", Toast.LENGTH_LONG).show();
-        } else {
-            if (correo.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Campo correo vacío", Toast.LENGTH_LONG).show();
-            } else {
-                if (telefono.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Campo teléfono vacío", Toast.LENGTH_LONG).show();
-                } else {
-                    if (pass.getText().toString().isEmpty()) {
-                        Toast.makeText(this, "Campo contraseña vacío", Toast.LENGTH_LONG).show();
-                    } else {
-                        if (!validarEmail(correo.getText().toString())) {
-                            Toast.makeText(this, "Correo incorrecto", Toast.LENGTH_LONG).show();
-                        } else {
-                            startActivity(new Intent(RegistroActivity.this, ExitosoActivity.class));
+        }else{
+            if(nombre.getText().toString().isEmpty()){
+                Toast.makeText(this, "Campo nombre vacío", Toast.LENGTH_LONG).show();
+            }else{
+                if(correo.getText().toString().isEmpty()){
+                    Toast.makeText(this, "Campo correo vacío", Toast.LENGTH_LONG).show();
+                }else{
+                    if(telefono.getText().toString().isEmpty()){
+                        Toast.makeText(this, "Campo teléfono vacío", Toast.LENGTH_LONG).show();
+                    }else{
+                        if(pass.getText().toString().isEmpty()){
+                            Toast.makeText(this, "Campo contraseña vacío", Toast.LENGTH_LONG).show();
+                        }else{
+                            if(!validarEmail(correo.getText().toString())){
+                                Toast.makeText(this, "Correo incorrecto", Toast.LENGTH_LONG).show();
+                            }else{
+                                registro();
+                            }
                         }
-
                     }
                 }
+            }
+        }
+    }*/
+
+    public void registro(){
+        Usuario user = new Usuario();
+        user.setIdentificador(AES.encrypt(cedula.getText().toString(),"64aes64"));
+        user.setNombre(AES.encrypt(nombre.getText().toString(),"64aes64"));
+        user.setEmail(AES.encrypt(correo.getText().toString(),"64aes64"));
+        user.setTelefono(AES.encrypt(telefono.getText().toString(),"64aes64"));
+        user.setContrasena(AES.encrypt(pass.getText().toString(),"64aes64"));
+        Call<Usuario> calluser = API.conexionUsuario().crearUsuario(user, authHeader);
+        calluser.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                try{
+                    if(response.isSuccessful()){
+                        registroDialogo();
+                    }
+                }catch(NullPointerException e){
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+
+            }
+        });
+
+    }
+    public void add(View v){
+        if(validacion()){
+            if(!validarEmail(correo.getText().toString())){
+                Toast.makeText(this, "Correo incorrecto", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this, "Procesando...", Toast.LENGTH_LONG).show();
+                registro();
             }
         }
     }
     private boolean validarEmail(String email) {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
+    }
+    private void registroDialogo(){
+        new AlertDialog.Builder(this)
+                .setTitle("Todo Listo!")
+                .setMessage("Registro Exitoso!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+    public boolean validacion(){
+        boolean retorno = true;
+        String c1 = cedula.getText().toString();
+        String c2 = nombre.getText().toString();
+        String c3 = correo.getText().toString();
+        String c4 = telefono.getText().toString();
+        String c5 = pass.getText().toString();
+        if(c1.isEmpty()){
+            cedula.setError("Introduzca su cédula!");
+            retorno=false;
+        }
+        if(c2.isEmpty()){
+            nombre.setError("Introduzca su nombre!");
+            retorno=false;
+        }
+        if(c3.isEmpty()){
+            correo.setError("Introduzca su correo!");
+            retorno=false;
+        }
+        if(c4.isEmpty()){
+            telefono.setError("Introduzca su número telefónico!");
+            retorno=false;
+        }
+        if(c5.isEmpty()){
+            pass.setError("Introduzca su contraseña!");
+            retorno=false;
+        }else{
+            registro();
+        }
+        return retorno;
+
     }
 }
